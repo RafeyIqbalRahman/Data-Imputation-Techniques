d49b1448a2b012b5ee165b84fde8af6c033102df
Rafey Iqbal Rahman <59226057+RafeyIqbalRahman@users.noreply.github.com>
2020-08-03 17:02:17 +0500

    Create KNNImputer.py

---
+++ KNNImputer.py
@@ -0,0 +1,37 @@
+from numpy import isnan, nan
+from pandas import read_csv, DataFrame
+from sklearn.impute import KNNImputer
+
+# Load the data
+df = read_csv('https://raw.githubusercontent.com/jbrownlee/Datasets/master/horse-colic.csv',
+              header=None,
+              na_values='?',)
+
+# Show the first 5 rows of the data
+df.head()
+
+# Define X (Predictor variables) and y (Target variable)
+dt = df.values
+ix = [i for i in range(dt.shape[1]) if i != 27]
+X, y = dt[:, ix], dt[:, 27]
+
+# Show count of missing values of X (before imputation)
+sum(isnan(X).flatten())
+
+# Define imputer
+imp = KNNImputer(missing_values=nan,
+                 n_neighbors=5,
+                 weights='distance',
+                 metric='nan_euclidean',)
+
+# Fit and transform imputer on the dataset
+Xtrans = imp.fit_transform(X)
+
+# Show count of missing values of Xtrans (after imputation)
+sum(isnan(Xtrans).flatten())
+
+# Convert NumPy array to Pandas DataFrame
+Xtrans = DataFrame(data=Xtrans)
+
+# Show the first 5 rows of the data with imputed values
+Xtrans.head()
